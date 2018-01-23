d84323d3b7d608b60676b352ce28b71e55467160
Fergal Moran <fergal.moran@gmail.com>
2018-01-23 15:36:04 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -62,7 +62,3 @@ sudo pip install livereload speedtest-cli virtualenv virtualenvwrapper
 sudo npm install -g goops
 
 chsh -s /bin/zsh
-
-mkdir -p /home/fergalm/.local/share/fonts
-cd /home/fergalm/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20for%20Powerline%20Nerd%20Font%20Complete.otf
-
